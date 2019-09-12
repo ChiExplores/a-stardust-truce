f1d38f0545fcdecaa022d367df369674298e6e03
Sorrel Bri <sorrel@SBJ-comp.local>
2019-09-12 09:58:56 -0700

    run mypy type checks, format init methods

--- ref_data.py
+++ ref_data.py
@@ -1,23 +1,11 @@
-class User:
-    __def__(self, name: str):
-        self.name = name
 
-class Data_Structure:
-    __def__(self, name: str, aggregate: dict, description: str):
+
+class User:
+    def __init__(self, name: str):
         self.name = name
-        self.description = description
-        self.__get_properties__():
-            pass
-        self.__get_methods__():
-            pass
-        self.__get_js__():
-            pass
-        self.__get_py__():
-            pass
-        self.data_aggregate = data_aggregate
 
 class Data_Aggregate:
-    __def__(self, name: str, type: str, dimension: int, code_block: dict, resource: dict):
+    def __init__(self, name: str, type: str, dimension: int, code_block: dict, resource: dict):
         # type selected from tuple ('node', 'block')
         self.name = name
         self.type = type
@@ -25,8 +13,22 @@ class Data_Aggregate:
         self.code_block = code_block
         self.resource = resource
 
+class Data_Structure:
+    def __init__(self, name: str, data_aggregate: Data_Aggregate, description: str):
+        self.name = name
+        self.description = description
+        self.data_aggregate = data_aggregate
+    def __get_properties__(self):
+        pass
+    def __get_methods__(self):
+        pass
+    def __get_js__(self):
+        pass
+    def __get_py__(self):
+        pass
+
 class Property:
-    __def__(self, name: str, type: str, code_block: dict, resource: dict):
+    def __init__(self, name: str, type: str, code_block: dict, resource: dict):
         # type selected from tuple ('int', 'str', 'float', 'var')
         self.name = name
         self.type = type
@@ -34,36 +36,36 @@ class Property:
         self.resource = resource
 
 class Method:
-    __def__(self, name: str, code_block: dict, resource: dict):
+    def __init__(self, name: str, code_block: dict, resource: dict):
         self.name = name
         self.code_block = code_block
         self.resource = resource
 
 class Code_Block:
-    __def__(self, python: str, javascript: str):
+    def __init__(self, python: str, javascript: str):
         self.python = python
         self.javascript = javascript
 
 class Resource:
-    __def__(self, description: str, img_url: str):
+    def __init__(self, description: str, img_url: str):
         self.description = description
         self.img_url = img_url
 
 # instantiations with reference to missing entity arguments
-single_link_node = Data_Aggregate('Singly Linked Node', 'node', 1) # code_block, resource
-head = Property('Head', 'var') # code_block, resource
-list_size = Property('List Size', 'int') # code_block, resource
-initialize = Method('Initialize') # code_block, resource
-push = Method('Push') # code_block, resource
-pop = Method('Pop') # code_block, resource
-peek = Method('Peek') # code_block, resource
-is_empty = Method('Is Empty') # code_block, resource
-stack_list = Structure('Stack - List', single_link_node, 'Linked List Implementation of Data Stack')
+single_link_node = Data_Aggregate('Singly Linked Node', 'node', 1, None, None) # code_block, resource
+head = Property('Head', 'var', None, None) # code_block, resource
+list_size = Property('List Size', 'int', None, None) # code_block, resource
+initialize = Method('Initialize', None, None) # code_block, resource
+push = Method('Push', None, None) # code_block, resource
+pop = Method('Pop', None, None) # code_block, resource
+peek = Method('Peek', None, None) # code_block, resource
+is_empty = Method('Is Empty', None, None) # code_block, resource
+stack_list = Data_Structure('Stack - List', single_link_node, 'Linked List Implementation of Data Stack')
 
 # dependency tuples for each method and property model eg
-push.agg_dependency = 'single_link_node', 'double_link_node'
-push.prop_dependency = 'head', 'last'
+# push.agg_dependency = 'single_link_node', 'double_link_node'
+# push.prop_dependency = 'head', 'last'
 
 # join table representations
-data_structure_property = {Stack: head, Stack: list_size}
-data_structure_method = {Stack: initialize, Stack: push, Stack: pop, Stack: peek, Stack: is_empty}
+data_structure_property = {stack_list: head, stack_list: list_size}
+data_structure_method = {stack_list: initialize, stack_list: push, stack_list: pop, stack_list: peek, stack_list: is_empty}
