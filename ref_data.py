07680b586705945a90601e706a73eac303157840
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-09-11 22:30:42 -0700

    stub class references for entities

---
+++ ref_data.py
@@ -0,0 +1,27 @@
+class User:
+    __def__():
+        pass
+
+class Data_Structure:
+    __def__():
+        pass
+
+class Data_Aggregate:
+    __def__():
+        pass
+
+class Property:
+    __def__():
+        pass
+
+class Method:
+    __def__():
+        pass
+
+class Code_Block:
+    __def__():
+        pass
+
+class Resource:
+    __def__():
+        pass
