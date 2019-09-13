bfc65873e0627cea210a7f06005ac2df44d11ebe
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-09-13 00:19:38 -0700

    add more items to dependencies dict

--- main_app/dependencies.py
+++ main_app/dependencies.py
@@ -1,17 +1,35 @@
+element = 'element'
+property = 'property'
+all_elements = 'single_link_node', 'double_link_list_node'
+all_properties = 'head', 'list_size'
+
 dependencies = {
-    properties: {
-        head: 'single_link_node', 'double_link_list_node',
-        list_size: 'single_link_node', 'double_link_list_node',
+    'properties': {
+        'head': 'single_link_node', 'double_link_list_node',
+        'list_size': 'single_link_node', 'double_link_list_node',
     },
-    methods: {
-        push_list: {
+    'methods': {
+        'push_list': {
             element: 'single_link_node', 'double_link_list_node',
             property: 'head',
         },
+        'pop_list': {
+            element: 'single_link_node', 'double_link_list_node',
+            property: 'head',
+        },
+        'peek_list': {
+            element: 'single_link_node', 'double_link_list_node',
+            property: 'head',
+        }
+        is_empty_list: {
+            element: 'single_link_node', 'double_link_list_node',
+            propery: all_properties
+        }
     }
 }
 
 # to search for dependencies of property
+# TODO make iterable
 # dependences['properities'][some_prop].get(data_structure['element'], 'That Property Can\'t Be added to this Data Structure!')
 # dependences['methods'][some_method]['element'].get(data_structure['element'], 'That Method Can\'t Be added to this Data Structure!')
 # dependences['methods'][some_method]['property'].get(data_structure['property'], 'That Method Can\'t Be added to this Data Structure!')
