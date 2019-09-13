81b0ef50ca95637f1efda7fba005ff891371bfe9
Chi Huynh <chi@skyhawk-277gt.local>
2019-09-13 13:19:11 -0700

    modified test files

---
+++ main_app/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, Client
+from django.http import HttpRequest
+# from django.test import SimpleTestCase
+# from django.urls import reverse
+from .views import *
+
+
+class ViewTest(TestCase):
+
+  def test_homepage(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code,200)
+
+  def test_aboutpage(self):
+    response = self.client.get('/about')
+    self.assertEqual(response.status_code, 200)
+
+  def test_structures(self):
+    response = self.client.get('/structures')
+    self.assertEqual(response.status_code, 200)
+
+  def test_structures_create(self):
+    response = self.client.get('/structures/create')
+    self.assertEqual(response.status_code, 200)
+
+
+# 401 unauthorized. User accessing things they should/'t
+#testing routes
+#looking at put request, and validating fields that we dont want to allow.
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,21 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+	path('', views.home, name='home'),
+	path('about/',views.home, name='home'), #not sure if we need this one?
+# 	path('structures/', views.structure_view, name='index'),
+# 	path('structures/create/', views.structure_create, name ='create'),
+# 	path('structures/<int:data_structures_id>/', views.structure_detail, name='detail'),
+# 	path('structures/<int:data_structures_id>/edit/', views.structure_edit, name='edit'),
+# 	path('structures/<int:data_structures_id>/assoc_element/<int:element_id>', views.assoc_element, name ='assoc_element'),
+# 	path('structures/<int:data_structures_id>/assoc_element/<int:element_id>/delete', views.delete_assoc_element, name ='delete_assoc_element'), 
+# 	path('structures/<int:data_structures_id>/assoc_prop/<int:prop_id>', views.assoc_prop, name ='assoc_prop'),
+# 	path('structures/<int:data_structures_id>/assoc_prop/<int:prop_id>/delete', views.delete_assoc_prop, name ='delete_assoc_prop'),
+# 	path('structures/<int:data_structures_id>/assoc_method/<int:method_id>', views.assoc_method, name ='assoc_method'),
+# 	path('structures/<int:data_structures_id>/assoc_method/<int:method_id>/delete', views.delete_assoc_method, name ='delete_assoc_method'),
 
-    # Account Functionality
-    path('accounts/signup', views.signup, name='signup'),
+
+	# Account Functionality
+	path('accounts/signup', views.signup, name='signup'),
+	
 ]
