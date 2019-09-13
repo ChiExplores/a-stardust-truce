2c4793df2c19728beab760a5e507d90947d31434
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-09-12 23:01:17 -0700

    create models for Code Block, Resource, Element

--- a_stardust_truce/models.py
+++ a_stardust_truce/models.py
@@ -1,1 +1,31 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Code_Block(models.Model):
+    python = models.TextField()
+    javascript = models.TextField()
+
+class Resource(models.Model):
+    description = models.TextField()
+    img_url = models.CharField()
+
+class Element(models.Model):
+    name = models.CharField(max_length=50)
+    NODE = 'NO'
+    BLOCK = 'BL'
+    TYPE_CHOICES = (
+        (NODE, 'Node'), # a data element that stores a referent to other elements
+        (BLOCK, 'Block') # a data element that stroes subelements in sequence
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=TYPE_CHOICES,
+        default=NODE
+    )
+    dimension = models.IntegerField(
+        MaxValueValidator(2), # can be increased later
+        MinValueValidator(1),
+        default=1
+    )
+    code_block = models.ForeignKey(Code_Block, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
