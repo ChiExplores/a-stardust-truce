1caf6609d4986fcfbea98381c10007266ed714f9
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-09-12 23:39:20 -0700

    add models for property, method, data_structure

--- a_stardust_truce/models.py
+++ a_stardust_truce/models.py
@@ -29,3 +29,33 @@ class Element(models.Model):
     )
     code_block = models.ForeignKey(Code_Block, on_delete=models.CASCADE)
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+
+class Property:
+    name = models.CharField(max_length=50)
+    STRING = 'str'
+    INTEGER = 'int'
+    FLOAT = 'float'
+    TYPE_CHOICES = (
+        (STRING, 'String'),
+        (INTEGER, 'Integer'),
+        (FLOAT, 'Float')
+    )
+    type = models.CharField(
+        max_length=5,
+        choices=TYPE_CHOICES,
+        default=INTEGER
+    )
+    code_block = models.ForeignKey(Code_Block, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+
+class Method:
+    name = models.CharField(max_length=50)
+    code_block = models.ForeignKey(Code_Block, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+
+class Data_Structure:
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=255)
+    element = models.ForeignKey(Element, on_delete=models.CASCADE)
+    properties = models.ManyToManyField(Property)
+    methods = models.ManyToManyField(Method)
