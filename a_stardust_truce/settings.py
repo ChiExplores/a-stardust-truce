55a735816aa68e22484f8dba21e32ad4ae69b5e9
Sorrel Bri <sorrel@SBJ-comp.hsd1.ca.comcast.net>
2019-09-12 23:07:55 -0700

    change database settings to postgreSQL

--- a_stardust_truce/settings.py
+++ a_stardust_truce/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'a_stardust_truce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'astardusttruce',
     }
 }
 
