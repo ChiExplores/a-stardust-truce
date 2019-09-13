ff4afecd4b88debb3aab25517642e486f04d7753
Sorrel Bri <sorrel@SBJ-comp.local>
2019-09-13 10:01:47 -0700

    debug dependencies dictionary entries, debug dictionary search

--- main_app/dependencies.py
+++ main_app/dependencies.py
@@ -1,35 +1,42 @@
 element = 'element'
 property = 'property'
-all_elements = 'single_link_node', 'double_link_list_node'
-all_properties = 'head', 'list_size'
+all_elements = ('single_link_node', 'double_link_list_node')
+all_properties = ('head', 'list_size')
 
 dependencies = {
     'properties': {
-        'head': 'single_link_node', 'double_link_list_node',
-        'list_size': 'single_link_node', 'double_link_list_node',
+        'head': ('single_link_node', 'double_link_list_node',),
+        'list_size': ('single_link_node', 'double_link_list_node',),
     },
     'methods': {
         'push_list': {
-            element: 'single_link_node', 'double_link_list_node',
-            property: 'head',
+            element: ('single_link_node', 'double_link_list_node',),
+            property: ('head',)
         },
         'pop_list': {
-            element: 'single_link_node', 'double_link_list_node',
-            property: 'head',
+            element: ('single_link_node', 'double_link_list_node',),
+            property: ('head',)
         },
         'peek_list': {
-            element: 'single_link_node', 'double_link_list_node',
-            property: 'head',
-        }
-        is_empty_list: {
-            element: 'single_link_node', 'double_link_list_node',
-            propery: all_properties
+            element: ('single_link_node', 'double_link_list_node',),
+            property: ('head',)
+        },
+        'is_empty_list': {
+            element: ('single_link_node', 'double_link_list_node',),
+            property: all_properties
         }
     }
 }
 
 # to search for dependencies of property
-# TODO make iterable
-# dependences['properities'][some_prop].get(data_structure['element'], 'That Property Can\'t Be added to this Data Structure!')
-# dependences['methods'][some_method]['element'].get(data_structure['element'], 'That Method Can\'t Be added to this Data Structure!')
-# dependences['methods'][some_method]['property'].get(data_structure['property'], 'That Method Can\'t Be added to this Data Structure!')
+
+# if data_structure['element'] in dependences['properities'][some_prop]:
+#     return 'That Property Can be added to this Data Structure!' 
+# else:
+#     return 'Sorry, that Property Can\'t Be added to this Data Structure!'
+
+# search for dependencies of method
+# if data_structure['element'] in dependences['methods'][some_method]['element'] and data_structure['property'] in dependences['methods'][some_method]['property']:
+#     return 'That Property Can be added to this Data Structure!'
+# else:
+#     return 'Sorry, that Property Can\'t Be added to this Data Structure!'
