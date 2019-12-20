a436eec0bf3032efa02461ecdae945629f646a3a
Chi Huynh <contact@chihuynh.me>
2019-12-20 14:46:39 -0800

    added CodeBlock serializer

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,12 +1,21 @@
-from .models import  DataStructure, Method, Property
+from .models import  DataStructure, Method, Property, CodeBlock
 from rest_framework import serializers
 
+
+class CodeBlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodeBlock
+        fields = ['id', 'name', 'python', 'javascript']
+
+
 class MethodSerializer(serializers.ModelSerializer):
+    code_block = CodeBlockSerializer(read_only=True)
     class Meta:
         model = Method
         fields = ['id', 'name', 'code_block', 'resource']
 
 class PropertySerializer(serializers.ModelSerializer):
+    code_block = CodeBlockSerializer(read_only=True)
     class Meta:
         model = Property
         fields = ['id', 'name', 'type', 'code_block', 'resource']
