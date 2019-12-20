08f4ab8c9b3255f687d46ce789c46bcce6565361
Chi Huynh <contact@chihuynh.me>
2019-12-20 15:06:40 -0800

    added serializer for Element model without the dimension property. Note: I can add the dimension prop if needed but it is unused

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,6 +1,10 @@
-from .models import  DataStructure, Method, Property, CodeBlock
+from .models import *
 from rest_framework import serializers
 
+class ResourceBlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resource
+        fields = ['id', 'name', 'description', 'img_url']
 
 class CodeBlockSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,21 +14,28 @@ class CodeBlockSerializer(serializers.ModelSerializer):
 
 class MethodSerializer(serializers.ModelSerializer):
     code_block = CodeBlockSerializer(read_only=True)
+    resource = ResourceBlockSerializer(read_only=True)
     class Meta:
         model = Method
         fields = ['id', 'name', 'code_block', 'resource']
 
 class PropertySerializer(serializers.ModelSerializer):
     code_block = CodeBlockSerializer(read_only=True)
+    resource = ResourceBlockSerializer(read_only=True)
     class Meta:
         model = Property
         fields = ['id', 'name', 'type', 'code_block', 'resource']
 
-
+class ElementSerializer(serializers.ModelSerializer):
+    code_block = CodeBlockSerializer(read_only=True)
+    class Meta:
+        model = Element
+        fields = ['name', 'type', 'code_block', 'resource']
 
 class DataStructureSerializer(serializers.ModelSerializer):
     methods = MethodSerializer(read_only=True, many=True)
     properties = PropertySerializer(read_only=True, many=True)
+    element = ElementSerializer(read_only=True)
     #Meta provide info about model
     class Meta:
         model = DataStructure
