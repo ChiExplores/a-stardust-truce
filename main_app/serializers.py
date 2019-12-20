a3dea61ceecb1702941669d478c5f4fa7d383566
Chi Huynh <contact@chihuynh.me>
2019-12-20 11:30:14 -0800

    creating many to many relationship serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,10 +1,25 @@
-from django.contrib.auth.models import User
+from .models import  DataStructure, Method, Property
 from rest_framework import serializers
-# from .models import User
 
-
-class DataStructureSerializer(serializers.HyperlinkedModelSerializer):
+class MethodSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['name', 'description', 'element',
+        model = Method
+        fields = ['id', 'name', 'code_block', 'resource']
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = ['id', 'name', 'type', 'code_block', 'resource']
+
+
+
+class DataStructureSerializer(serializers.ModelSerializer):
+    methods = MethodSerializer(read_only=True, many=True)
+    properties = PropertySerializer(read_only=True, many=True)
+    #Meta provide info about model
+    class Meta:
+        model = DataStructure
+        fields = ['id', 'name', 'description', 'element',
                   'properties', 'methods', 'user']
+
+
