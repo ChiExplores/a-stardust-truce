321a9730e35e3b771a861a0097c02ab9f5d567c5
Chi Huynh <contact@chihuynh.me>
2019-12-20 15:21:05 -0800

    added User serializer with username

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,15 +1,22 @@
 from .models import *
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['username']
 
 class ResourceBlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
-        fields = ['id', 'name', 'description', 'img_url']
+        fields = ['name', 'description', 'img_url']
 
 class CodeBlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = CodeBlock
-        fields = ['id', 'name', 'python', 'javascript']
+        fields = ['name', 'python', 'javascript']
 
 
 class MethodSerializer(serializers.ModelSerializer):
@@ -17,29 +24,32 @@ class MethodSerializer(serializers.ModelSerializer):
     resource = ResourceBlockSerializer(read_only=True)
     class Meta:
         model = Method
-        fields = ['id', 'name', 'code_block', 'resource']
+        fields = ['name', 'code_block', 'resource']
 
 class PropertySerializer(serializers.ModelSerializer):
     code_block = CodeBlockSerializer(read_only=True)
     resource = ResourceBlockSerializer(read_only=True)
     class Meta:
         model = Property
-        fields = ['id', 'name', 'type', 'code_block', 'resource']
+        fields = ['name', 'type', 'code_block', 'resource']
 
 class ElementSerializer(serializers.ModelSerializer):
     code_block = CodeBlockSerializer(read_only=True)
+    resource = ResourceBlockSerializer(read_only=True)
     class Meta:
         model = Element
         fields = ['name', 'type', 'code_block', 'resource']
 
-class DataStructureSerializer(serializers.ModelSerializer):
+class DataStructureSerializer(serializers.ModelSerializer): 
     methods = MethodSerializer(read_only=True, many=True)
     properties = PropertySerializer(read_only=True, many=True)
     element = ElementSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+    
     #Meta provide info about model
     class Meta:
         model = DataStructure
-        fields = ['id', 'name', 'description', 'element',
+        fields = ['name', 'description', 'element',
                   'properties', 'methods', 'user']
 
 
