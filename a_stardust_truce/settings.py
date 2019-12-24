9fe90c1444f4a17cb912a0853be6ae3f793adeb3
Chi Huynh <contact@chihuynh.me>
2019-12-23 17:03:45 -0800

    added REST_FRAMEWORK configuration.

--- a_stardust_truce/settings.py
+++ a_stardust_truce/settings.py
@@ -54,6 +54,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {  
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 ROOT_URLCONF = 'a_stardust_truce.urls'
 
 TEMPLATES = [
