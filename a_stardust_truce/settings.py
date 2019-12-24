7adcd46cb442169dad06d7bb3c457fc4a1377436
Chi Huynh <contact@chihuynh.me>
2019-12-23 17:26:56 -0800

    added test format to json property in REST_FRAMEWORK config

--- a_stardust_truce/settings.py
+++ a_stardust_truce/settings.py
@@ -59,7 +59,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 ROOT_URLCONF = 'a_stardust_truce.urls'
